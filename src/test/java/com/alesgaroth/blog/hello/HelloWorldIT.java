148331aeb9cc59834014a177ede4113c63419334
Alan Linton <alesgaroth@gmail.com>
2024-02-20 16:53:49 -0500

    Move stop port off the same port used for http

--- src/test/java/com/alesgaroth/blog/hello/HelloWorldIT.java
+++ src/test/java/com/alesgaroth/blog/hello/HelloWorldIT.java
@@ -2,11 +2,12 @@ package com.alesgaroth.blog.hello;
 
 import org.htmlunit.html.HtmlPage;
 import org.htmlunit.WebClient;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldIT {
   @Test
-  public void homePage() throws Exception {
+  public void localhost() throws Exception {
     try (final WebClient webClient = new WebClient()) {
       final HtmlPage page = webClient.getPage("http://localhost:8080/");
     }
